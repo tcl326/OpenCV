f11a20a9bf155ce598b7f5b9c73351f34153339b
Ting-Che Lin <tcl326@nyu.edu>
2015-12-17 19:28:40 -0500

    Removing unnecessary couts

--- GlobalMotionEstimation/GlobalMotionEstimation/main.cpp
+++ GlobalMotionEstimation/GlobalMotionEstimation/main.cpp
@@ -23,20 +23,11 @@ void transformPerspective (std::vector<cv::Point2f> original, std::vector<cv::Po
     {
         x = original[i].x;
         y = original[i].y;
-        cout << x << "," << y << endl;
-        cout << m.at<float>(0,0);
         temp = Point2f((m.at<float>(0,0)*x+m.at<float>(0,1)*y+m.at<float>(0, 2))/(m.at<float>(2,0)*x+m.at<float>(2,1)*y+m.at<float>(2, 2)),(m.at<float>(1,0)*x+m.at<float>(1,1)*y+m.at<float>(1, 2))/(m.at<float>(2,0)*x+m.at<float>(2,1)*y+m.at<float>(2, 2)));
-        cout << temp;
         dst.push_back(temp);
     }
 }
 
-int addition (int a, int b)
-{
-    int r;
-    r=a+b;
-    return r;
-}
 
 
 int main(){
@@ -48,26 +39,9 @@ int main(){
     by.push_back(Point2f(3,3));
     by.push_back(Point2f(3,4));
     Mat t = videostab::estimateGlobalMotionLeastSquares(ax,by,3,0);
-    cout << t;
-    Mat c;
-    Mat cvt(by);
-    cout << cvt<<endl;
-    Mat cvt1;
-    Mat t1;
-    //cvt.convertTo(cvt1, CV_32FC2);
-    //t.convertTo(t1, CV_32FC2);
-    //cout << t.size() << t1.size() << endl;
-    //cout << cvt.reshape(1,0).size() << cvt1.t().size() << endl;
-    //c = t*cvt.reshape(1,0);
-    //Mat dst;
-    //warpPerspective(cvt, dst, t, dst.size());
-    int z;
-    z = addition (5,3);
-    cout << z;
 
     transformPerspective(ax, cz, t);
     cout << cz;
-    //cout << dst;
-    //cout << c;
+
     return 0;
 }
