315ae89a202c9fe2bc9e8f06d20b63b6acddda32
Ting-Che Lin <tcl326@nyu.edu>
2016-02-04 09:48:45 -0500

    fusion complete

--- RiverSegmentation/RiverSegmentation/main.cpp
+++ RiverSegmentation/RiverSegmentation/main.cpp
@@ -72,6 +72,8 @@ double calcMinRadius(vector<Point2f>& points)
 
 // Calculate entropy using minimum radius found using default OpenCV function, inacurate as any radius
 // smaller than 1 will return 1
+
+/*
 double calcEntropy(double length, vector<Point2f>& points, float &maxRadius)
 {
     if (points.size() < 2){
@@ -101,6 +103,19 @@ double calcEntropyUsingMiniballRadius (double length, vector<Point2f>& points, f
     return entropy;
 }
 
+
+
+// calculate the distance between two points
+double calcLength(Point2f p1, Point2f p2)
+{
+    double length;
+    Point2f difference;
+    difference = p1-p2;
+    length = sqrt(pow(difference.x,2)+pow(difference.y,2));
+    return length;
+}
+*/
+
 void calcEntropyUsingMiniballRadiusIterative (double& entropy, double& length, vector<Point2f>& points, float &maxRadius)
 {
     if (points.size() < 2){
@@ -115,15 +130,7 @@ void calcEntropyUsingMiniballRadiusIterative (double& entropy, double& length, v
     }
 }
 
-// calculate the distance between two points
-double calcLength(Point2f p1, Point2f p2)
-{
-    double length;
-    Point2f difference;
-    difference = p1-p2;
-    length = sqrt(pow(difference.x,2)+pow(difference.y,2));
-    return length;
-}
+
 void calcLengthIterative(double& length, Point2f& p1, Point2f& p2)
 {
     Point2f difference;
@@ -140,12 +147,135 @@ void entropyListUpdate (vector<double>& entropy, vector<double>& lengths, vector
     }
 }
 
+//Dissimilarity
+
+double euclideanDistance (Point2f point1, Point2f point2){
+    double distance;
+    Point2f p;
+    p = point1 - point2;
+    distance = sqrt( p.x*p.x + p.y*p.y );
+    return distance;
+}
+
+Point2f angleDistance (Point2f point1, Point2f point2){
+    double distance;
+    double angle;
+    Point2f angleDistance;
+    Point2f p;
+    p = point2 - point1;
+    distance = sqrt( p.x*p.x + p.y*p.y );
+    angle = atan(p.y/p.x);
+    angleDistance.x = angle;
+    angleDistance.y = distance;
+    return angleDistance;
+}
+
+vector<int> findNeighbourIndexIterative (int givenTrackerIndex, std::vector<cv::Point2f> points){
+    double radius;
+    radius = 10.0;
+    vector<int> neighbourIndex;
+    size_t n = points.size();
+    while (neighbourIndex.size() < 4) {
+        neighbourIndex = {};
+        for (int i = 0; i < n; i++){
+            if (i == givenTrackerIndex) {
+                continue;
+            }
+            if (radius >= euclideanDistance(points[givenTrackerIndex], points[i])){
+                neighbourIndex.push_back(i);
+            }
+        }
+        radius++;
+    }
+    cout << radius << endl;
+    return neighbourIndex;
+}
+
+void findNeighbourIndexList (vector< vector<int> >& neighbourIndexList, std::vector<cv::Point2f> points){
+    for (int i = 0; i < points.size(); i++){
+        neighbourIndexList[i] = findNeighbourIndexIterative(i, points);
+    }
+}
+
+void updateNeighbour (vector< vector<int> >& neighbourIndexList, vector<int> removedIndexList){
+    bool remove = false;
+    for (int k = 0; k < removedIndexList.size(); k++){
+        for (int i = 0; i < neighbourIndexList.size(); i++) {
+            for (int j = 0; j < neighbourIndexList[i].size(); j++) {
+                if (neighbourIndexList[i][j] > removedIndexList[k]) {
+                    neighbourIndexList[i][j]--;
+                }
+                if (neighbourIndexList[i][j] == removedIndexList[k]) {
+                    remove = true;
+                }
+            }
+            if (remove) {
+                neighbourIndexList[i].erase(std::remove(neighbourIndexList[i].begin(), neighbourIndexList[i].end(), removedIndexList[k]), neighbourIndexList[i].end());
+                remove = false;
+                
+            }
+        }
+    }
+}
+/*
+ void dStarIterativeUpdate (int givenTrackerIndex, vector<int>& neighbourIndex, std::vector< std::vector<cv::Point2f> >& allTrackers, vector<double>& dStarList){
+ double euclideanDis = 0.0;
+ size_t n = neighbourIndex.size();
+ for (int i=0; i<n; i++){
+ euclideanDis += euclideanDistance(angleDistance( allTrackers[givenTrackerIndex].back(), allTrackers[givenTrackerIndex].end()[-2]), angleDistance (allTrackers[neighbourIndex[i]].back(), allTrackers[neighbourIndex[i]].end()[-2]));
+ }
+ dStarList[givenTrackerIndex] += (euclideanDis/n);
+ }
+ */
+
+void dStarListIterativeUpdate (vector< vector<int> >& neighbourIndexList, std::vector< std::vector<cv::Point2f> >& allTrackers, vector<double>& dStarList){
+    
+    //cout << "[ ";
+    for (int j = 0; j < allTrackers.size(); j++){
+        double euclideanDis = 0.0;
+        size_t n = neighbourIndexList[j].size();
+        for (int i=0; i<n; i++){
+            euclideanDis += euclideanDistance(allTrackers[j].back(), allTrackers[neighbourIndexList[j][i]].back());
+        }
+        dStarList[j] += (euclideanDis/n);
+        //cout << dStarList[j] << ", ";
+    }
+    //cout << "]" << endl;
+}
+
+
+void mininimumDissimilarityIterative (int givenTrackerIndex , vector<int>& neighbourIndex, std::vector< std::vector<cv::Point2f> >& allTrackers, vector<double>& dStarList, vector<double>& minDissimilarity){
+    double dissimilarity;
+    size_t n = neighbourIndex.size();
+    //dStarIterativeUpdate(givenTrackerIndex, neighbourIndex, allTrackers, dStarList);
+    minDissimilarity[givenTrackerIndex] = dStarList[neighbourIndex[0]];
+    for (int i=1; i<n; i++){
+        dissimilarity = dStarList[neighbourIndex[i]];
+        if (minDissimilarity[givenTrackerIndex] > dissimilarity){
+            minDissimilarity[givenTrackerIndex] = dissimilarity;
+        }
+    }
+}
+
+void updateDissimilarityIterative (std::vector< std::vector<cv::Point2f> >& allTrackers, vector<double>& dStarList, vector<double>& minDissimilarity, vector< vector<int> >& neighbourIndexList){
+    for (int i = 0; i < allTrackers.size(); i++){
+        mininimumDissimilarityIterative(i, neighbourIndexList[i], allTrackers, dStarList, minDissimilarity);
+    }
+}
+
+void fusionUpdate (vector<double>& entropyList, vector<double>& dStarList, vector<double>& fusionList){
+    for (int i = 0; i < entropyList.size(); i++){
+        fusionList[i] = entropyList[i] * dStarList[i];
+    }
+}
 
 int main(int argc, const char* argv[])
 {
 
     char* movie;
-    movie = "/Users/student/Desktop/OpenCV/RiverSegmentation/RiverSegmentation/MovieBoat.mp4";
+    movie = "/Users/student/Desktop/GP058145.m4v";
+    //GP058145.m4v"
+    //OpenCV/RiverSegmentation/RiverSegmentation/MovieBoat.mp4";
     VideoCapture cap;
     TermCriteria termcrit(CV_TERMCRIT_ITER|CV_TERMCRIT_EPS, 20, 0.03);
     Size subPixWinSize(10,10), winSize(31,31);
@@ -198,6 +328,14 @@ int main(int argc, const char* argv[])
     vector<double> entropy(MAX_COUNT);
     vector<double> naturalBreaks;
     
+    //Variables for Dissimilarity
+    vector< vector<int> > neighbourIndexList(MAX_COUNT);
+    vector<double> minDissimilarity (MAX_COUNT);
+    vector<double> dissimilarity (MAX_COUNT);
+    vector<double> dStarList (MAX_COUNT);
+    vector<double> radiuses (MAX_COUNT);
+    vector<double> fusion (MAX_COUNT);
+    
     float maxRadius = 0.0;
     int c = 0;
     
@@ -218,12 +356,14 @@ int main(int argc, const char* argv[])
             goodFeaturesToTrack(gray, points[1], MAX_COUNT, 0.01, 10, Mat(), 3, 0, 0.04);
             cornerSubPix(gray, points[1], subPixWinSize, Size(-1,-1), termcrit);
             addRemovePt = false;
+            findNeighbourIndexList(neighbourIndexList, points[1]);
         }
         
         else if( !points[0].empty() )
         {
             vector<uchar> status;
             vector<float> err;
+            std::vector<int> removedIndex;
             if(prevGray.empty())
                 gray.copyTo(prevGray);
 
@@ -234,13 +374,18 @@ int main(int argc, const char* argv[])
             {
                 
                 if( !status[i] )
-                    
+                {
+                    removedIndex.push_back(i);
                     continue;
+                }
                 points[0][k] = points[0][i];
                 points[1][k] = points[1][i];
                 tracking[k] = tracking[i];
                 lengths[k] = lengths[i];
                 tracking[k].push_back(points[1][i]);
+                dStarList[k] = dStarList[i];
+                minDissimilarity[k] = minDissimilarity[i];
+                neighbourIndexList[k] = neighbourIndexList[i];
                 
                 /*
                 if (c != 0)
@@ -273,8 +418,15 @@ int main(int argc, const char* argv[])
             tracking.resize(k);
             entropy.resize(k);
             lengths.resize(k);
-            
+            dStarList.resize(k);
+            minDissimilarity.resize(k);
+            neighbourIndexList.resize(k);
+            fusion.resize(k);
+            updateNeighbour(neighbourIndexList, removedIndex);
+            dStarListIterativeUpdate(neighbourIndexList, tracking, dStarList);
+            updateDissimilarityIterative(tracking, dStarList, minDissimilarity, neighbourIndexList);
             entropyListUpdate(entropy, lengths, tracking, maxRadius);
+            fusionUpdate(entropy,dStarList,fusion);
             
             c += 1;
         }
@@ -287,27 +439,27 @@ int main(int argc, const char* argv[])
             
             if (c > 3)
             {
-                naturalBreaks = JenksNaturalBreak(entropy,4);
+                naturalBreaks = JenksNaturalBreak(fusion,4);
                 
-                for (int p = 0; p<entropy.size(); ++p)
+                for (int p = 0; p<fusion.size(); ++p)
                 {
-                    if (entropy[p] < naturalBreaks[0])
+                    if (fusion[p] < naturalBreaks[0])
                     {
                         // Draw Blue Circles
                         circle( image, tracking[p].rbegin()[0], 3, Scalar(255,0,0), -1, 8);
                     }
-                    else if (entropy[p] < naturalBreaks[1])
+                    else if (fusion[p] < naturalBreaks[1])
                     {
                         // Draw Green Circles
                         circle( image, tracking[p].rbegin()[0], 3, Scalar(0,255,0), -1, 8);
                     }
-                    else if (entropy[p] < naturalBreaks[2])
+                    else if (fusion[p] < naturalBreaks[2])
                     {
                         //Draw Red Circles
                         circle( image, tracking[p].rbegin()[0], 3, Scalar(0,0,255), -1, 8);
                     }
 
-                    else if (entropy[p] < naturalBreaks[3])
+                    else if (fusion[p] < naturalBreaks[3])
                     {
                         //Draw Purple Circles
                         circle( image, tracking[p].rbegin()[0], 3, Scalar(255,0,255), -1, 8);
