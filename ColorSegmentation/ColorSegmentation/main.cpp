df935a376d9de413c4842069798aefd23efe426e
Ting-Che Lin <tcl326@nyu.edu>
2016-05-09 12:24:23 -0400

    finsihing

--- ColorSegmentation/ColorSegmentation/main.cpp
+++ ColorSegmentation/ColorSegmentation/main.cpp
@@ -1,11 +1,77 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/video/tracking.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/videostab/videostab.hpp"
 
+#include <vector>
 #include <iostream>
+#include <string.h>
+#include <map>
+#include <fstream>
 
 using namespace cv;
 using namespace std;
+
+void readPoint (vector<Point2f>& points1, vector<Point2f>& point0, const char* filename, const int& number){
+    char tempFilename[100];
+    char *token;
+    string line;
+    strcpy(tempFilename,filename);
+    sprintf(tempFilename, strcat(tempFilename, "%03d.txt"), number);
+    ifstream textFile;
+    textFile.open(tempFilename);
+    if (textFile.is_open()){
+        getline(textFile, line);
+        while (getline(textFile, line)){
+            Point2f tempPoint;
+            int tempInt;
+            char tokenArray[line.size()+1];//as 1 char space for null is also required
+            strcpy(tokenArray, line.c_str());
+            token = strtok(tokenArray, " ,");
+            string strToken(token);
+            tempPoint.x = stof(strToken);
+            token = strtok(NULL, " ,");
+            strToken = string(token);
+            tempPoint.y = stof(strToken);
+            token = strtok(NULL, " ,");
+            strToken = string(token);
+            tempInt = stoi(strToken);
+            if (tempInt){
+                points1.push_back(tempPoint);
+            }
+            else{
+                point0.push_back(tempPoint);
+            }
+        }
+    }
+}
+
+void readImage (Mat& image, const char* filename, const int& number){
+    char tempFilename[100];
+    strcpy(tempFilename,filename);
+    sprintf(tempFilename, strcat(tempFilename, "%03d.jpg"), number);
+    image = imread(tempFilename);
+}
+
+void getFrame(Mat& frame, int i){
+    char frameImage[100];
+    strcpy(frameImage, "/Users/student/Desktop/OpenCV/testForShoreline/frame");
+    readImage(frame, frameImage, i);
+}
+
+void getTrackerImage (Mat& trackerImage, int i){
+    char trackerImageFile[100];
+    strcpy(trackerImageFile, "/Users/student/Desktop/OpenCV/testForShoreline/tracker");
+    readImage(trackerImage, trackerImageFile, i);
+}
+
+void getPointValue (vector<Point2f>& pointWithValue1, vector<Point2f>& pointWithValue0, int i){
+    char textCSVFile[100];
+    strcpy(textCSVFile, "/Users/student/Desktop/OpenCV/testForShoreline/currentPointAndValues");
+    readPoint(pointWithValue1, pointWithValue0, textCSVFile, i);
+}
 
 static void help(char** argv)
 {
@@ -17,57 +83,83 @@ static void help(char** argv)
 }
 
 //This colors the segmentations
-static void floodFillPostprocess( Mat& img, const Scalar& colorDiff=Scalar::all(1) )
+static void floodFillPostprocess( Mat& img, vector<Point2f>& pointValue1, vector<Point2f>& pointValue0 , const Scalar& colorDiff=Scalar::all(1))
 {
     CV_Assert( !img.empty() );
     RNG rng = theRNG();
     Mat mask( img.rows+2, img.cols+2, CV_8UC1, Scalar::all(0) );
-    for( int y = 0; y < img.rows; y++ )
-    {
-        for( int x = 0; x < img.cols; x++ )
-        {
-            if( mask.at<uchar>(y+1, x+1) == 0 )
-            {
-                Scalar newVal( rng(256), rng(256), rng(256) );
-                floodFill( img, mask, Point(x,y), newVal, 0, colorDiff, colorDiff );
-            }
+//    Scalar ocean, land;
+//    ocean = Scalar(0,0,255);
+//    land = Scalar(0,255,0);
+//    for( int y = 0; y < img.rows; y++ )
+//    {
+//        for( int x = 0; x < img.cols; x++ )
+//        {
+//            if( mask.at<uchar>(y+1, x+1) == 0 )
+//            {
+//                Scalar newVal( rng(256), rng(256), rng(256) );
+//                floodFill( img, mask, Point(x,y), newVal, 0, colorDiff, colorDiff );
+//            }
+//        }
+//    }
+    Scalar ocean, land;
+    ocean = Scalar(0,0,255);
+    land = Scalar(0,255,0);
+    for (int i = 0; i < pointValue0.size(); i++) {
+        if (pointValue0[i].x > img.cols || pointValue0[i].y > img.rows){
+            continue;
         }
+        floodFill( img, mask, pointValue0[i], ocean, 0, colorDiff, colorDiff );
     }
+    for (int i = 0; i < pointValue1.size(); i++) {
+        if (pointValue1[i].x > img.cols || pointValue1[i].y > img.rows){
+            continue;
+        }
+        floodFill( img, mask, pointValue1[i], land, 0, colorDiff, colorDiff );
+    }
+
 }
 
-string winName = "meanshift";
-int spatialRad, colorRad, maxPyrLevel;
-Mat img, res;
 
-static void meanShiftSegmentation( int, void* )
+
+static void meanShiftSegmentation(Mat& img, Mat& res, int& spatialRad, int& colorRad, int&maxPyrLevel, string& winName, vector<Point2f>& pointValue1, vector<Point2f>& pointValue0)
 {
     cout << "spatialRad=" << spatialRad << "; "
     << "colorRad=" << colorRad << "; "
     << "maxPyrLevel=" << maxPyrLevel << endl;
     pyrMeanShiftFiltering( img, res, spatialRad, colorRad, maxPyrLevel );
-    floodFillPostprocess( res, Scalar::all(2) );
+    
+    imshow("res", res);
+    floodFillPostprocess( res, pointValue1, pointValue0 , Scalar::all(2));
     imshow( winName, res );
 }
 
 int main(int argc, char** argv)
 {
-
+    string winName = "meanshift";
+    int spatialRad, colorRad, maxPyrLevel;
+    Mat img, res;
+    int i = 0;
+    vector<Point2f> pointWithValue1, pointWithValue0;
+    getPointValue(pointWithValue1, pointWithValue0, i);
     
-    img = imread( "/Users/student/Desktop/CV Canal/framePyramid.jpg" );
+    
+    getFrame(img, i);
     if( img.empty() )
         return -1;
     
-    spatialRad = 10;
-    colorRad = 10;
-    maxPyrLevel = 1;
+    spatialRad = 5;
+    colorRad = 20;
+    maxPyrLevel = 2;
     
     namedWindow( winName, WINDOW_AUTOSIZE );
+    namedWindow("res", WINDOW_AUTOSIZE);
     
-    createTrackbar( "spatialRad", winName, &spatialRad, 80, meanShiftSegmentation );
-    createTrackbar( "colorRad", winName, &colorRad, 60, meanShiftSegmentation );
-    createTrackbar( "maxPyrLevel", winName, &maxPyrLevel, 5, meanShiftSegmentation );
+//    createTrackbar( "spatialRad", winName, &spatialRad, 80, meanShiftSegmentation );
+//    createTrackbar( "colorRad", winName, &colorRad, 60, meanShiftSegmentation );
+//    createTrackbar( "maxPyrLevel", winName, &maxPyrLevel, 5, meanShiftSegmentation );
     
-    meanShiftSegmentation(0, 0);
+    meanShiftSegmentation(img,res,spatialRad,colorRad,maxPyrLevel,winName, pointWithValue1, pointWithValue0);
     waitKey();
     return 0;
 }
---
+++ LawMask/LawMask/main.cpp
@@ -0,0 +1,346 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+void readPoint (vector<Point2f>& points1, vector<Point2f>& point0, const char* filename, const int& number){
+    char tempFilename[100];
+    char *token;
+    string line;
+    strcpy(tempFilename,filename);
+    sprintf(tempFilename, strcat(tempFilename, "%03d.txt"), number);
+    ifstream textFile;
+    textFile.open(tempFilename);
+    if (textFile.is_open()){
+        getline(textFile, line);
+        while (getline(textFile, line)){
+            Point2f tempPoint;
+            int tempInt;
+            char tokenArray[line.size()+1];//as 1 char space for null is also required
+            strcpy(tokenArray, line.c_str());
+            token = strtok(tokenArray, " ,");
+            string strToken(token);
+            tempPoint.x = stof(strToken);
+            token = strtok(NULL, " ,");
+            strToken = string(token);
+            tempPoint.y = stof(strToken);
+            token = strtok(NULL, " ,");
+            strToken = string(token);
+            tempInt = stoi(strToken);
+            if (tempInt){
+                points1.push_back(tempPoint);
+            }
+            else{
+                point0.push_back(tempPoint);
+            }
+        }
+    }
+}
+
+void readImage (Mat& image, const char* filename, const int& number){
+    char tempFilename[100];
+    strcpy(tempFilename,filename);
+    sprintf(tempFilename, strcat(tempFilename, "%03d.jpg"), number);
+    image = imread(tempFilename, CV_LOAD_IMAGE_COLOR);
+}
+
+void getFrame(Mat& frame, int i){
+    char frameImage[100];
+    strcpy(frameImage, "/Users/student/Desktop/OpenCV/testForShoreline/frame");
+    readImage(frame, frameImage, i);
+}
+
+void getTrackerImage (Mat& trackerImage, int i){
+    char trackerImageFile[100];
+    strcpy(trackerImageFile, "/Users/student/Desktop/OpenCV/testForShoreline/tracker");
+    readImage(trackerImage, trackerImageFile, i);
+}
+
+void getPointValue (vector<Point2f>& pointWithValue1, vector<Point2f>& pointWithValue0, int i){
+    char textCSVFile[100];
+    strcpy(textCSVFile, "/Users/student/Desktop/OpenCV/testForShoreline/currentPointAndValues");
+    readPoint(pointWithValue1, pointWithValue0, textCSVFile, i);
+}
+
+static void floodFillPostprocess( Mat& img, vector<Point2f>& pointValue1, vector<Point2f>& pointValue0 , const Scalar& colorDiff=Scalar::all(2))
+{
+    CV_Assert( !img.empty() );
+    RNG rng = theRNG();
+    Mat mask( img.rows+2, img.cols+2, CV_8UC1, Scalar::all(0) );
+
+    Scalar ocean, land;
+    ocean = Scalar(0,0,255);
+    land = Scalar(0,255,0);
+    for (int i = 0; i < pointValue0.size(); i++) {
+        if (pointValue0[i].x > img.cols || pointValue0[i].y > img.rows){
+            continue;
+        }
+        floodFill( img, mask, pointValue0[i], ocean, 0, colorDiff, colorDiff );
+    }
+    for (int i = 0; i < pointValue1.size(); i++) {
+        if (pointValue1[i].x > img.cols || pointValue1[i].y > img.rows){
+            continue;
+        }
+        floodFill( img, mask, pointValue1[i], land, 0, colorDiff, colorDiff );
+    }
+    
+}
+
+void show_result(const cv::Mat& labels, const cv::Mat& centers, int height, int width, const vector<Vec3b>& colorTable, vector<Point2f>& point1, vector<Point2f>& point0)
+{
+    std::cout << "===\n";
+    std::cout << "labels: " << labels.rows << " " << labels.cols << std::endl;
+    std::cout << "centers: " << centers.rows << " " << centers.cols << std::endl;
+    assert(labels.type() == CV_32SC1);
+    assert(centers.type() == CV_32FC1);
+    
+    cv::Mat rgb_image(height, width, CV_8UC3);
+    cv::MatIterator_<cv::Vec3b> rgb_first = rgb_image.begin<cv::Vec3b>();
+    cv::MatIterator_<cv::Vec3b> rgb_last = rgb_image.end<cv::Vec3b>();
+    cv::MatConstIterator_<int> label_first = labels.begin<int>();
+    
+//    cv::Mat centers_u8;
+//    centers.convertTo(centers_u8, CV_8UC1, 255.0);
+//    cv::Mat centers_u8c3 = centers_u8.reshape(3);
+    
+    while ( rgb_first != rgb_last ) {
+        const cv::Vec3b& rgb = (colorTable[*label_first]);
+        *rgb_first = rgb;
+        ++rgb_first;
+        ++label_first;
+    }
+    Mat final;
+    
+    //pyrUp(rgb_image, final, Size( rgb_image.cols*2, rgb_image.rows*2 ));
+    
+    //floodFillPostprocess(rgb_image, point1, point0);
+    
+    cv::imshow("tmp", rgb_image);
+    cv::waitKey();
+}
+
+static Vec3b randomColor( RNG& rng )
+{
+    int icolor = (unsigned) rng;
+    return Vec3b( icolor&255, (icolor>>8)&255, (icolor>>16)&255 );
+}
+
+void generateColorTable (vector<Vec3b>& colorTable, const int& numberOfColors){
+    RNG rng(0xFFFFFFFF);
+    for (int i = 0; i < numberOfColors; i++) {
+        colorTable.push_back(randomColor(rng));
+    }
+}
+
+void generateLawMasks (vector<Mat>& LawMasks){
+    Mat L5, E5, S5, R5, W5;
+    
+    L5 = (Mat_<float>(1,5) << 1,4,6,4,1);
+    E5 = (Mat_<float>(1,5) << -1,-2,0,2,1);
+    S5 = (Mat_<float>(1,5) << -1,0,2,0,-1);
+    R5 = (Mat_<float>(1,5) << 1,-4,6,-4,1);
+    W5 = (Mat_<float>(1,5) << -1,2,0,-2,1);
+    
+    
+    Mat L5E5, L5S5, L5R5, E5E5, E5L5, E5S5, E5R5, S5S5, S5L5, S5R5, S5E5, R5R5, R5L5, R5E5, R5S5, M1, M2, M3, M4, M5 ,M6;
+    
+    L5E5 = L5.t()*E5;
+    E5L5 = L5E5.t();
+    L5S5 = L5.t()*S5;
+    S5L5 = L5S5.t();
+    L5R5 = L5.t()*R5;
+    R5L5 = L5R5.t();
+    E5E5 = E5.t()*E5;
+    E5S5 = E5.t()*S5;
+    S5E5 = E5S5.t();
+    E5R5 = E5.t()*R5;
+    R5E5 = E5R5.t();
+    S5S5 = S5.t()*S5;
+    S5R5 = S5.t()*R5;
+    R5S5 = S5R5.t();
+    R5R5 = R5.t()*R5;
+    
+    addWeighted(L5E5, 0.5, E5L5, 0.5, 0, M1);
+    LawMasks.push_back(M1);
+    addWeighted(L5S5, 0.5, S5L5, 0.5, 0, M2);
+    LawMasks.push_back(M2);
+    addWeighted(L5R5, 0.5, R5L5, 0.5, 0, M3);
+    LawMasks.push_back(M3);
+    addWeighted(E5S5, 0.5, S5E5, 0.5, 0, M4);
+    LawMasks.push_back(M4);
+    addWeighted(E5R5, 0.5, R5E5, 0.5, 0, M5);
+    LawMasks.push_back(M5);
+    addWeighted(S5R5, 0.5, R5R5, 0.5, 0, M6);
+    LawMasks.push_back(M6);
+//    LawMasks.push_back(L5E5);
+//    LawMasks.push_back(E5L5);
+//    LawMasks.push_back(L5S5);
+//    LawMasks.push_back(S5L5);
+//    LawMasks.push_back(L5R5);
+//    LawMasks.push_back(R5L5);
+//    LawMasks.push_back(E5S5);
+//    LawMasks.push_back(S5E5);
+//    LawMasks.push_back(E5R5);
+//    LawMasks.push_back(R5E5);
+//    LawMasks.push_back(S5R5);
+//    LawMasks.push_back(R5R5);
+    
+    
+    LawMasks.push_back(S5S5);
+    LawMasks.push_back(E5E5);
+    LawMasks.push_back(R5R5);
+}
+
+/** @function main */
+int main ()
+{
+    /// Declare variables
+    vector<Mat> dsts;
+    
+    Mat dst, integral, colorSrc, frame, smallFrame;
+    
+    Mat kernel;
+    Point anchor;
+    double delta;
+    int ddepth;
+    int kernel_size;
+    char* window_name = "filter2D Demo";
+    
+    int c;
+    
+    // Law's Mask
+    
+    int frameNumber = 0;
+    
+    vector<Mat> kernels;
+    
+    generateLawMasks(kernels);
+    
+    getFrame(frame, frameNumber);
+    
+    //pyrDown( frame, smallFrame, Size( frame.cols/2, frame.rows/2 ));
+    
+    //imshow("frame", frame);
+    
+    cv::Mat greyMat, colorMat, averageIntensity, intensityRemoved;
+    cv::cvtColor(frame, greyMat, CV_BGR2GRAY);
+
+    //src = imread("/Users/student/Desktop/OpenCV/testForShoreline/frame000.jpg",CV_LOAD_IMAGE_GRAYSCALE);
+    
+    if( !frame.data )
+    { return -1; }
+    
+    /// Create window
+    namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+    
+    /// Initialize arguments for the filter
+    anchor = Point( -1, -1 );
+    delta = 0;
+    ddepth = -1;
+    
+    boxFilter(greyMat, averageIntensity, CV_8UC1, Size (15,15), anchor);
+    
+//    imshow("greyMat", greyMat);
+//    
+//    imshow("averageInte", averageIntensity);
+    
+    intensityRemoved = greyMat - averageIntensity;
+    
+    //imshow("removed", intensityRemoved);
+    
+    // Loop - Will filter the image with different kernel sizes each 0.5 seconds
+    int ind = 0;
+    while( ind < kernels.size() )
+    {
+        Mat result, sumResult;
+        c = waitKey(1000);
+        
+        /// Update kernel size for a normalized box filter
+        kernel = kernels[ind];
+        
+        filter2D(intensityRemoved, result, CV_32F , kernel, anchor, delta, BORDER_DEFAULT );
+        boxFilter(result, sumResult, CV_32F, Size (15,15), anchor, false, BORDER_DEFAULT);
+        //cout << integral;
+        imshow( window_name, sumResult );
+        dsts.push_back(sumResult);
+        ind++;
+    }
+//
+    vector<Mat> bgrChannels, maxGBRBRG, c1c2c3;
+    
+    split(frame, bgrChannels);
+    
+    //Mat maxGB, maxRB, maxRG;
+    maxGBRBRG.push_back(max(bgrChannels[1], bgrChannels[0]));//, maxGB);
+    maxGBRBRG.push_back(max(bgrChannels[2], bgrChannels[0]));//, maxRB);
+    maxGBRBRG.push_back(max(bgrChannels[2], bgrChannels[1]));//, maxRG);
+    
+    
+//    //Mat c1(maxGB.rows,maxGB.cols,CV_32FC1), c2(maxRB.rows,maxRB.cols,CV_32FC1), c3(maxRG.rows,maxRG.cols,CV_32FC1);
+    
+    MatIterator_<float> it, end;
+    MatIterator_<uchar> maxIt, maxEnd, frameIt, frameEnd;
+    
+    for (int i = 0; i < bgrChannels.size(); i++) {
+        Mat max = maxGBRBRG[i];
+        Mat c1(max.rows,max.cols,CV_32FC1);
+        it = c1.begin<float>();
+        maxIt = max.begin<uchar>();
+        frameIt = bgrChannels[i].begin<uchar>();
+        for (; it != c1.end<float>(); ) {
+            *it = fastAtan2(float(*frameIt),float( *maxIt))*255/360;
+            //cout << *it << endl;
+            it++;
+            maxIt++;
+            frameIt++;
+        }
+//        imshow("c1c2c3", c1);
+//        imshow("bgr", bgrChannels[i]);
+//        c = waitKey(1000);
+        dsts.push_back(c1);
+    }
+    
+    Mat height(greyMat.rows,greyMat.cols,CV_32FC1);
+    MatIterator_<float> heightIt;
+    
+    heightIt = height.begin<float>();
+    
+    for (int c = 0; heightIt != height.end<float>(); heightIt++, c++) {
+        *heightIt = int(c)/height.cols;
+        //cout << *heightIt << endl;
+    }
+    dst.push_back(height);
+    
+    vector<Mat> points;
+    
+    for (int i = 0; i < dsts.size(); i++) {
+        Mat reshaped;
+        reshaped = dsts[i].reshape(1,dsts[i].rows*dsts[i].cols);
+        points.push_back(reshaped);
+    }
+    
+    Mat labels, centers, sample;
+    
+    hconcat(points, sample);
+    
+    cv::TermCriteria criteria {cv::TermCriteria::COUNT, 100, 1};
+    
+    kmeans(sample, 10, labels, criteria, 1, KMEANS_RANDOM_CENTERS, centers);
+    
+    vector<Vec3b> colorTable;
+    generateColorTable(colorTable, 32);
+    
+    vector<Point2f> points1, points0;
+    
+    getPointValue(points1, points0, frameNumber);
+    
+    show_result(labels, centers, greyMat.rows, greyMat.cols, colorTable, points1, points0);
+    
+    
+    return 0;
+}
